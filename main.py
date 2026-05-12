aa1b0a4802d698016b3da0ede2045d29bf355f5a
Dennis Kibet Kemboi <dkemboi2003@gmail.com>
2026-05-12 12:22:13 +0300

    Built core CLI command structure

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Task Tracker CLI Started")
+import sys
+
+if len(sys.argv) < 2:
+    print("Please give a command: ")
+else:
+    command = sys.argv[1]
+    
+    if command == 'add':
+        if len(sys.argv) < 3:
+            print("Please provide a task description:")
+        else:
+            description = sys.argv[2]
+            print("Adding task:", description)
+            
+    elif command == 'list':
+        print("Listing tasks..")
+        
+    elif command == 'delete':
+        print("Deleting task..")
+        
+    else:
+        print("Unknown command.")
+        
+
