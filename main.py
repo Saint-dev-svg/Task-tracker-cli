8a8b5f1e95637f3bdcf6cacdfabe8c47e5cdbb2c
Dennis Kibet Kemboi <dkemboi2003@gmail.com>
2026-05-12 15:25:30 +0300

    Implemented task Add feature

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import json
 import os
+from datetime import datetime
 
 TASKS_FILE = "tasks.json"
 
@@ -18,3 +19,36 @@ def save_tasks(tasks):
     with open(TASKS_FILE, "w") as file:
         json.dump(tasks, file, indent = 4)
         
+def generate_task_id(tasks):
+    if not tasks:
+        return 1
+    return tasks[-1]["id"] +1
+
+if len(sys.argv) < 2:
+    print("Please enter a command:")
+else:
+    command = sys.argv[1]
+    
+    if command == 'add':
+        if len(sys.argv) < 3:
+            print("Please give a task description:")
+        
+        else:
+            description = sys.argv[2]
+            tasks = load_tasks()
+            task_id = generate_task_id(tasks)
+            
+            current_time = datetime.now().isoformat()
+            
+            new_task = {
+                "id" : task_id,
+                "description" : description,
+                "status" : "to-do",
+                "created At" : current_time,
+                "Updated At" : current_time
+            }
+            
+            tasks.append(new_task)
+            save_tasks(tasks)
+            
+            print("Task added successfully. ID:", task_id)
